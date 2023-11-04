b3b5c53d0169c4289213a4aeb280a4ed332941c3
Lucas Gentile <lucasgentile@live.com>
2023-11-04 02:01:40 -0300

    initial commit
    criado áreas iniciais

---
+++ pages/includes/footer/footer.php
@@ -0,0 +1,84 @@
+<footer>
+  <div class="about-city-bg">
+    <article class="container">
+      <!-- Area Inicial  -->
+      <div class="d-lg-flex justify-content-between history-city pt-lg-5">
+
+        <figure class="history-image order-2">
+          <img src="./assets/images/foto-cidade.png" alt="foto da cidade" class="img-fluid pt-5 pt-lg-0 pb-3">
+        </figure>
+
+        <div class="d-lg-flex flex-column order-1 history-text">
+          <h2 class="text-light text-center">São Roque: A Terra do Vinho</h2>
+          <p class="text-light text-center">Uma cidade de economia diversificada com atividades no segmento industrial, comercial e na área de serviços. Devido ao seu legado histórico e cultural, à sua exuberante natureza (40% do território do município é coberto por Mata Atlântica), o município foi elevado à condição de Estância Turística em 1990.</p>
+          <a href="">
+            <p class="text-success text-center fw-bold mt-4 mb-4">Continue Lendo...</p>
+          </a>
+        </div>
+      </div>
+
+
+      <!-- Botoes -->
+      <div class="pb-5">
+        <ul class="text-light d-flex overflow-auto scroll gap-1">
+          <li class="button d-flex align-items-center fw-bold p-3">Historia de São Roque</li>
+          <li class="d-flex align-items-center fw-bold p-3">Feiras</li>
+          <li class="d-flex align-items-center fw-bold p-3">Centros Culturais</li>
+          <li class="d-flex align-items-center fw-bold p-3">Biblioteca</li>
+          <li class="d-flex align-items-center fw-bold p-3">Telefones de Emergência</li>
+          <li class="d-flex align-items-center fw-bold p-3">Turismo</li>
+          <li class="d-flex align-items-center fw-bold p-3">Galeria de Ex-prefeitos</li>
+          <li class="d-flex align-items-center fw-bold p-3">Mais +</li>
+        </ul>
+      </div>
+    </article>
+  </div>
+
+  <!-- Segunda Parte -->
+
+  <!-- Aplicativos // Todos -->
+  <div class="start-content">
+    <article class="container">
+
+      <div>
+        <!-- Logo Prefeitura -->
+        <div>
+          <img class="d-md-none d-lg-block d-none pt-lg-5" src="./assets/images/logo-sr-branco.png" alt="logo sao roque com letras brancas">
+        </div>
+
+        <!-- Apps -->
+        <div class="text-light pt-5 pt-lg-5 pb-5">
+          <h3>Baixe nosso App</h3>
+          <div class="apple d-inline-flex align-items-center gap-2 lh-1 btn-app">
+            <i class="bi bi-apple"></i>
+            <p>Baixar no <br> App Store</p>
+          </div>
+
+          <div class="android d-inline-flex align-items-center gap-2 lh-1 btn-app">
+            <i class="bi bi-android2"></i>
+            <p>Baixar no <br> Google Play</p>
+          </div>
+        </div>
+
+        <!-- Redes Sociais -->
+
+        <div class="text-light pb-5">
+          <h3>Redes Sociais</h3>
+          <ul class="d-flex gap-3">
+            <li><a href="#"></a><i class="bi bi-facebook"></i></li>
+            <li><a href="#"></a><i class="bi bi-instagram"></i></li>
+            <li><a href="#"></a><i class="bi bi-youtube"></i></li>
+            <li><a href="#"></a><i class="bi bi-twitter-x"></i></li>
+          </ul>
+        </div>
+      </div>
+
+
+
+
+    </article>
+  </div>
+
+  <!-- Conteudo do meio // Apenas Desktop -->
+
+</footer>
