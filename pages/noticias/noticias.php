80d9d234becb19b634fdb3fdf6eb44e7c9737ad1
Lucas Gentile <lucasgentile@live.com>
2023-11-12 21:27:02 -0300

    add: criado a seção noticias

---
+++ pages/noticias/noticias.php
@@ -0,0 +1,112 @@
+<?php
+include '../../includes/head/head.php';
+?>
+
+<div class="container">
+  <!-- Header da Area de Noticias -->
+  <header class="justify-content-between d-md-none d-lg-block d-none p-3">
+    <div class="d-flex justify-content-between">
+      <ul class="d-flex gap-3">
+        <li><a href="../../index.php">Site Principal</a></li>
+        <li><a href="#">Serviços</a></li>
+        <li><a href="#">Fale Conosco</a></li>
+      </ul>
+
+
+      <ul class="d-flex gap-3">
+        <li><a href="#"><i class="bi bi-facebook"></i> Facebook</a></li>
+        <li><a href="#"><i class="bi bi-instagram"></i> Instagram</a></li>
+        <li><a href="#"><i class="bi bi-youtube"></i> Youtube</a></li>
+        <li><a href="#"><i class="bi bi-twitter-x"></i> Twitter</a></li>
+      </ul>
+    </div>
+  </header>
+
+  <nav class="d-flex justify-content-between">
+    <figure>
+      <img src="../../assets/images/noticias-logo.png" alt="logo-noticias" class="img-fluid pt-3">
+    </figure>
+
+    <ul class="d-flex align-items-center gap-4">
+      <li><a href="#">Inicio</a></li>
+      <li><a href="#">Noticias</a></li>
+      <li><a href="#">Banco de Imagens</a></li>
+    </ul>
+  </nav>
+
+  <!-- Banner -->
+  <div class="banner rounded-4 ">
+    <h3 class="text-light text-center p-3 fw-bolder">Seja bem-vindo(a) à Agência de Notícias da Prefeitura de São Roque</h3>
+  </div>
+
+  <!-- Noticias -->
+  <div class="row">
+    <!-- Noticia 1 -->
+    <div class="col-12 col-md-4">
+      <img src="../../assets/images/noticia-image-1.jpeg" alt="" class="img-fluid">
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, ipsam?</h1>
+      <p></p>
+    </div>
+
+
+
+    <!-- Noticia 2 -->
+    <div class="col-12 col-md-4">
+      <img src="../../assets/images/noticia-image-2.jpeg" alt="" class="img-fluid">
+      <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit, delectus?</h1>
+      <p></p>
+    </div>
+
+    <!-- Noticia 3 -->
+    <div class="col-12 col-md-4">
+      <img src="../../assets/images/noticia-image-3.jpeg" alt="" class="img-fluid">
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, ut?</h1>
+      <p></p>
+    </div>
+  </div>
+
+  <!-- Acontecendo Agora -->
+  <div class="bg-now p-4">
+    <h3 class="text-center text-primary fw-bold">Acontecendo Agora</h3>
+
+    <div class="row ">
+
+      <div class="col">
+        <div class="card d-flex flex-row bg-transparent border-0">
+          <img src="../../assets/images/noticias-agora-1.jpeg" alt="" class="img-fluid img-resize-now">
+          <div class="d-flex flex-column p-1">
+            <p class="card-text">Prefeitura de Goiânia alerta para alta temperatura que pode atingir a Capital neste domingo (12/11), com possibilidade de chegar a 38°C</p>
+            <small>Atualizado em:</small>
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class=" card d-flex flex-row bg-transparent border-0">
+          <img src="../../assets/images/noticias-agora-1.jpeg" alt="" class="img-fluid img-resize-now">
+          <div class="d-flex flex-column p-1">
+            <p class="card-text">Prefeitura de Goiânia alerta para alta temperatura que pode atingir a Capital neste domingo (12/11), com possibilidade de chegar a 38°C</p>
+            <small>Atualizado em:</small>
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card d-flex flex-row bg-transparent border-0">
+          <img src="../../assets/images/noticias-agora-1.jpeg" alt="" class="img-fluid img-resize-now">
+          <div class="d-flex flex-column p-1">
+            <p class="card-text">Prefeitura de Goiânia alerta para alta temperatura que pode atingir a Capital neste domingo (12/11), com possibilidade de chegar a 38°C</p>
+            <small>Atualizado em:</small>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+
+
+
+
+
+  </div>
+</div>
