e071f6012f3ede618192757577f74f89ddf8af75
Lucas Gentile <lucasgentile@live.com>
2023-11-13 16:37:18 -0300

    update: pagina noticias estrutura finalizada
    necessario estilização, texto e imagens

--- pages/noticias/noticias.php
+++ pages/noticias/noticias.php
@@ -24,7 +24,7 @@ include '../../includes/head/head.php';
 
   <nav class="d-flex justify-content-between">
     <figure>
-      <img src="../../assets/images/noticias-logo.png" alt="logo-noticias" class="img-fluid pt-3">
+      <img src="https://placehold.co/250x140" alt="logo-noticias" class="img-fluid pt-3">
     </figure>
 
     <ul class="d-flex align-items-center gap-4">
@@ -66,47 +66,254 @@ include '../../includes/head/head.php';
   </div>
 
   <!-- Acontecendo Agora -->
-  <div class="bg-now p-4">
-    <h3 class="text-center text-primary fw-bold">Acontecendo Agora</h3>
+  <section class="bg-now mt-4 rounded-3">
+    <h3 class="text-center text-primary fw-bold pt-3">Acontecendo Agora</h3>
 
-    <div class="row ">
+    <article class="row ">
 
-      <div class="col">
+      <section class="col m-3">
         <div class="card d-flex flex-row bg-transparent border-0">
-          <img src="../../assets/images/noticias-agora-1.jpeg" alt="" class="img-fluid img-resize-now">
+          <img src="https://placehold.co/100x100" alt="" class="img-fluid img-resize-now">
           <div class="d-flex flex-column p-1">
             <p class="card-text">Prefeitura de Goiânia alerta para alta temperatura que pode atingir a Capital neste domingo (12/11), com possibilidade de chegar a 38°C</p>
             <small>Atualizado em:</small>
           </div>
         </div>
-      </div>
+      </section>
 
-      <div class="col">
+      <section class="col m-3">
         <div class=" card d-flex flex-row bg-transparent border-0">
-          <img src="../../assets/images/noticias-agora-1.jpeg" alt="" class="img-fluid img-resize-now">
+          <img src="https://placehold.co/100x100" alt="" class="img-fluid img-resize-now">
           <div class="d-flex flex-column p-1">
             <p class="card-text">Prefeitura de Goiânia alerta para alta temperatura que pode atingir a Capital neste domingo (12/11), com possibilidade de chegar a 38°C</p>
             <small>Atualizado em:</small>
           </div>
         </div>
-      </div>
+      </section>
 
-      <div class="col">
+      <section class="col m-3">
         <div class="card d-flex flex-row bg-transparent border-0">
-          <img src="../../assets/images/noticias-agora-1.jpeg" alt="" class="img-fluid img-resize-now">
+          <img src="https://placehold.co/100x100" alt="" class="img-fluid img-resize-now">
           <div class="d-flex flex-column p-1">
             <p class="card-text">Prefeitura de Goiânia alerta para alta temperatura que pode atingir a Capital neste domingo (12/11), com possibilidade de chegar a 38°C</p>
             <small>Atualizado em:</small>
           </div>
         </div>
+      </section>
+
+      <section class="col m-3">
+        <div class="card d-flex flex-row bg-transparent border-0">
+          <img src="https://placehold.co/100x100" alt="" class="img-fluid img-resize-now">
+          <div class="d-flex flex-column p-1">
+            <p class="card-text">Prefeitura de Goiânia alerta para alta temperatura que pode atingir a Capital neste domingo (12/11), com possibilidade de chegar a 38°C</p>
+            <small>Atualizado em:</small>
+          </div>
+        </div>
+      </section>
+
+      <section class="col m-3">
+        <div class="card d-flex flex-row bg-transparent border-0">
+          <img src="https://placehold.co/100x100" alt="" class="img-fluid img-resize-now">
+          <div class="d-flex flex-column p-1">
+            <p class="card-text">Prefeitura de Goiânia alerta para alta temperatura que pode atingir a Capital neste domingo (12/11), com possibilidade de chegar a 38°C</p>
+            <small>Atualizado em:</small>
+          </div>
+        </div>
+      </section>
+
+      <section class="col m-3">
+        <div class="card d-flex flex-row bg-transparent border-0">
+          <img src="https://placehold.co/100x100" alt="" class="img-fluid img-resize-now">
+          <div class="d-flex flex-column p-1">
+            <p class="card-text">Prefeitura de Goiânia alerta para alta temperatura que pode atingir a Capital neste domingo (12/11), com possibilidade de chegar a 38°C</p>
+            <small>Atualizado em:</small>
+          </div>
+        </div>
+      </section>
+
+      <section class="col m-3">
+        <div class="card d-flex flex-row bg-transparent border-0">
+          <img src="https://placehold.co/100x100" alt="" class="img-fluid img-resize-now">
+          <div class="d-flex flex-column p-1">
+            <p class="card-text">Prefeitura de Goiânia alerta para alta temperatura que pode atingir a Capital neste domingo (12/11), com possibilidade de chegar a 38°C</p>
+            <small>Atualizado em:</small>
+          </div>
+        </div>
+      </section>
+
+      <section class="col m-3">
+        <div class="card d-flex flex-row bg-transparent border-0">
+          <img src="https://placehold.co/100x100" alt="" class="img-fluid img-resize-now">
+          <div class="d-flex flex-column p-1">
+            <p class="card-text">Prefeitura de Goiânia alerta para alta temperatura que pode atingir a Capital neste domingo (12/11), com possibilidade de chegar a 38°C</p>
+            <small>Atualizado em:</small>
+          </div>
+        </div>
+      </section>
+
+      <section class="col m-3">
+        <div class="card d-flex flex-row bg-transparent border-0">
+          <img src="https://placehold.co/100x100" alt="" class="img-fluid img-resize-now">
+          <div class="d-flex flex-column p-1">
+            <p class="card-text">Prefeitura de Goiânia alerta para alta temperatura que pode atingir a Capital neste domingo (12/11), com possibilidade de chegar a 38°C</p>
+            <small>Atualizado em:</small>
+          </div>
+        </div>
+      </section>
+
+      <section class="col m-3">
+        <div class="card d-flex flex-row bg-transparent border-0">
+          <img src="https://placehold.co/100x100" alt="" class="img-fluid img-resize-now">
+          <div class="d-flex flex-column p-1">
+            <p class="card-text">Prefeitura de Goiânia alerta para alta temperatura que pode atingir a Capital neste domingo (12/11), com possibilidade de chegar a 38°C</p>
+            <small>Atualizado em:</small>
+          </div>
+        </div>
+      </section>
+
+      <section class="col m-3">
+        <div class="card d-flex flex-row bg-transparent border-0">
+          <img src="https://placehold.co/100x100" alt="" class="img-fluid img-resize-now">
+          <div class="d-flex flex-column p-1">
+            <p class="card-text">Prefeitura de Goiânia alerta para alta temperatura que pode atingir a Capital neste domingo (12/11), com possibilidade de chegar a 38°C</p>
+            <small>Atualizado em:</small>
+          </div>
+        </div>
+      </section>
+
+      <section class="col m-3">
+        <div class="card d-flex flex-row bg-transparent border-0">
+          <img src="https://placehold.co/100x100" alt="" class="img-fluid img-resize-now">
+          <div class="d-flex flex-column p-1">
+            <p class="card-text">Prefeitura de Goiânia alerta para alta temperatura que pode atingir a Capital neste domingo (12/11), com possibilidade de chegar a 38°C</p>
+            <small>Atualizado em:</small>
+          </div>
+        </div>
+      </section>
+
+      <section class="col m-3">
+        <div class="card d-flex flex-row bg-transparent border-0">
+          <img src="https://placehold.co/100x100" alt="" class="img-fluid img-resize-now">
+          <div class="d-flex flex-column p-1">
+            <p class="card-text">Prefeitura de Goiânia alerta para alta temperatura que pode atingir a Capital neste domingo (12/11), com possibilidade de chegar a 38°C</p>
+            <small>Atualizado em:</small>
+          </div>
+        </div>
+      </section>
+
+      <section class="col m-3">
+        <div class="card d-flex flex-row bg-transparent border-0">
+          <img src="https://placehold.co/100x100" alt="" class="img-fluid img-resize-now">
+          <div class="d-flex flex-column p-1">
+            <p class="card-text">Prefeitura de Goiânia alerta para alta temperatura que pode atingir a Capital neste domingo (12/11), com possibilidade de chegar a 38°C</p>
+            <small>Atualizado em:</small>
+          </div>
+        </div>
+      </section>
+
+      <section class="col m-3">
+        <div class="card d-flex flex-row bg-transparent border-0">
+          <img src="https://placehold.co/100x100" alt="" class="img-fluid img-resize-now">
+          <div class="d-flex flex-column p-1">
+            <p class="card-text">Prefeitura de Goiânia alerta para alta temperatura que pode atingir a Capital neste domingo (12/11), com possibilidade de chegar a 38°C</p>
+            <small>Atualizado em:</small>
+          </div>
+        </div>
+      </section>
+
+    </article>
+  </section>
+
+  <!-- Informativos -->
+  <aside class="mt-4 d-flex flex-column flex-lg-row justify-content-center align-items-center gap-1">
+    <figure>
+      <img src="https://placehold.co/650x300" alt="" class="img-fluid rounded-3">
+    </figure>
+
+    <figure>
+      <img src="https://placehold.co/650x300" alt="" class="img-fluid rounded-3">
+    </figure>
+  </aside>
+
+  <!-- App e + -->
+  <div class="row">
+
+    <div class=" col-4 rounded-4 text-center">
+      <h3>Baixe o App <br>Prefeitura 24horas</h3>
+      <p>Gestão Inteligente tem <br>planejamento e tecnologia <br> para cuidar de você.</p>
+
+      <div>
+        <button>
+          <h3>Baixar no <br> Google Play</h3>
+        </button>
       </div>
 
+      <div>
+        <button>
+          <h3>Baixar na <br> App Store</h3>
+        </button>
+      </div>
+
+      <small>A Prefeitura de Goiânia iniciou o pré-cadastramento para as etapas de vacinação contra a Covid-19, que será feito exclusivamente pelo aplicativo.
+      </small>
     </div>
 
-
-
-
-
+    <div class="col-8">
+      <figure>
+        <img src="https://placehold.co/900x400" alt="" class="img-fluid rounded-4">
+      </figure>
+    </div>
 
   </div>
+
+  <!-- Cards Serviços -->
+  <div class="d-flex justify-content-between">
+    <div>
+      <i class="bi bi-bookmark-star"></i>
+      <h3>Diário Oficial de São Roque</h3>
+      <p>Consulta de documentos e publicações por Ano, Número e Assunto.</p>
+      <button>Acesse</button>
+    </div>
+
+    <div>
+      <i class="bi bi-gear-fill"></i>
+      <h3>Portal de Serviços</h3>
+      <p>Lista de todos os serviços oferecidos pelos órgãos públicos de São Roque.</p>
+      <button>Acesse</button>
+    </div>
+
+    <div>
+      <i class="bi bi-sign-intersection-y-fill"></i>
+      <h3>Estrutura Organizacional</h3>
+      <p>Os representantes e gestores de nosso município.</p>
+      <button>Acesse</button>
+    </div>
+
+    <div>
+      <i class="bi bi-info-circle-fill"></i>
+      <h3>Portal da Transparencia</h3>
+      <p>Acompanhe a execução orçamentária das ações do governo.</p>
+      <button>Acesse</button>
+    </div>
+
+  </div>
+
+  <footer class="pt-5">
+    <div class="d-flex justify-content-between align-items-center">
+      <img src="https://placehold.co/200x120" alt="logo-prefeitura">
+
+      <div>
+        <ul class="d-flex justify-content-center gap-5">
+          <li><a href=""><i class="bi bi-facebook"></i>Facebook</a></li>
+          <li><a href=""><i class="bi bi-instagram"></i>Instagram</a></li>
+          <li><a href=""><i class="bi bi-youtube"></i>Youtube</a></li>
+          <li><a href=""><i class="bi bi-twitter-x"></i>Twitter</a></li>
+        </ul>
+      </div>
+
+      <h4>Desenvolvidor por:</h4>
+    </div>
+  </footer>
+
+
 </div>
