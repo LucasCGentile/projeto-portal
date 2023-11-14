10846c92cbd5334e975976824c26f57e6b33f9b5
Lucas Gentile <lucasgentile@live.com>
2023-11-14 00:00:37 -0300

    update: atualizacao do header atualicao de algumas imagens do header e footer

--- includes/header/header.php
+++ includes/header/header.php
@@ -2,8 +2,81 @@
   <ul class="d-flex justify-content-center gap-5 list-unstyled">
     <li><a href="/projeto-portal/index.php">Home</a></li>
     <li><a href="/projeto-portal/pages/noticias/noticias.php">Noticias</a></li>
-    <li><a href=""></a></li>
-    <li><a href=""></a></li>
-    <li><a href=""></a></li>
+    <li><a href="/projeto-portal/pages/eventos/eventos.php">Eventos</a></li>
+    <li><a href="/projeto-portal/pages/turismo/turismo.php">Turismo</a></li>
+    <li><a href="/projeto-portal/pages/onibus/onibus.php">Horario Onibus</a></li>
   </ul>
 </header>
+
+<div class="bg-light d-flex flex-column justify-content-center align-items-center">
+  <div class="container text-center">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="row">
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item"><a class="nav-link active" aria-current="page" href="#"> INÍCIO </a>
+            </li>
+
+            <li class="nav-item dropdown"><a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"> INFORMAÇÕES </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">HORÁRIO DE ÔNIBUS</a></li>
+                <li><a class="dropdown-item" href="#">HISTÓRIA</a></li>
+              </ul>
+            </li>
+
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"> GUIA DE EVENTOS </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">TURISMO</a></li>
+                <li><a class="dropdown-item" href="#">EVENTOS</a></li>
+              </ul>
+            </li>
+          </ul>
+
+          <a class="navbar-brand" href="#"><img src="../../../projeto-portal/includes/header/assets/logo-sr-header.png" alt=" "></a>
+
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item dropdown"><a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"> OUVIDORIA </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">CONTATO</a></li>
+              </ul>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#"> REGRAS DE USO </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#"> QUEM SOMOS</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </div>
+
+
+  <div class="container text-center">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="row">
+        <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+      <li class="facebook">
+        <a href="https://facebook.com/"><i class="bi bi-facebook"></i>
+        </a>
+      </li>
+      <li class="instagram">
+        <a href="https://instagram.com/"><i class="bi bi-instagram"></i>
+        </a>
+      </li>
+      <li class="twitter">
+        <a href="https://twitter.com/"><i class="bi bi-twitter-x"></i>
+        </a>
+      </li>
+  </div>
+</div>
