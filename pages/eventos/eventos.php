9ac6b06fd9aba53c94cf9cb0bf2eac87d9a79a49
Lucas Gentile <lucasgentile@live.com>
2023-11-13 23:59:02 -0300

    add: criado area de eventos

---
+++ pages/eventos/eventos.php
@@ -0,0 +1,51 @@
+<?php
+include '../../includes/head/head.php'
+?>
+
+<?php
+include '../../includes/header/header.php'
+?>
+
+<body>
+
+  <div class="container">
+    <section>
+      <br>
+      <p>Eventos acontecendo em São Roque em OUTUBRO:</p>
+      <br>
+    </section>
+
+
+    <div class="seletor">
+      <div>
+        <p><a href="#" target="_blank"><b> OUTRUBRO </b></a></p>
+      </div>
+      <div>
+        <p><a href="#" target="_blank"><b> NOVEMBRO </b></a></p>
+      </div>
+      <div>
+        <p><a href="#" target="_blank"><b> DEZEMBRO </b></a></p>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="gallery-container">
+        <a href="imagens/Rectangle 71.png" class="gallery-items"><img src="../eventos/assets/Rectangle 72.png" alt=" "> <br> <span class="description">São Roque sedia o VII Encontro Paulista de Agroecologia</span></a>
+        <a href="imagens/Rectangle 32.png" class="gallery-items"><img src="../eventos/assets/Rectangle 32.png" alt=" "><span class="description">Castramóvel realiza ação para pets no próximo fim de semana em Maylasky</span></a>
+        <a href="imagens/Rectangle 72.png" class="gallery-items"><img src="../eventos/assets/Rectangle 72.png" alt=" "><span class="description">Feirão do Estância Supermercados acontece nesta terça em São Roque</span></a>
+        <a href="imagens/Rectangle 73.png" class="gallery-items"><img src="../eventos/assets/Rectangle 73.png" alt=" "><span class="description">23ª Festival da Pisa da uva de São Roque, acontece no próximo final de semana em Camguera.</span></a>
+        <a href="imagens/Rectangle 74.png" class="gallery-items"><img src="../eventos/assets/Rectangle 74.png" alt=" "><span class="description">Mercado Livre realiza novo feirão do emprego em São Roque</span></a>
+        <a href="imagens/Rectangle 75.png" class="gallery-items"><img src="../eventos/assets/Rectangle 75.png" alt=" "><span class="description">Feira Permanente recebe, neste domingo, Feira de Adoção</span></a>
+      </div>
+    </div>
+  </div>
+
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-
+C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+
+<?php
+include '../../includes/footer/footer.php'
+?>
