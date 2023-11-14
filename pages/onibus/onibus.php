e51df427559cf94f88bd33f1647bf7e6f67184b6
Lucas Gentile <lucasgentile@live.com>
2023-11-14 15:22:16 -0300

    update: area de onibis e onibus adicionado includes

--- pages/onibus/onibus.php
+++ pages/onibus/onibus.php
@@ -1,14 +1,9 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Horários de Ônibus de São Roque</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
-  <link rel="stylesheet" href="css/style.css" />
-</head>
+<?php
+include '../../includes/head/head.php';
+?>
 
 <body>
   <main>
@@ -107,7 +102,10 @@
       </div>
     </div>
   </main>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+  <?php
+  include '../../includes/footer/footer.php';
+  ?>
 </body>
 
 </html>
