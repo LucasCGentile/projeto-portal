eccec71b85dcd827ceb2fb373845ffdbd7dd1763
Lucas Gentile <lucasgentile@live.com>
2023-11-14 15:05:25 -0300

    add: criação do header

--- includes/header/header.php
+++ includes/header/header.php
@@ -1,85 +1,55 @@
-<header class="container text-center">
-  <ul class="d-flex justify-content-center gap-5 list-unstyled">
-    <li><a href="/projeto-portal/index.php">Home</a></li>
-    <li><a href="/projeto-portal/pages/noticias/noticias.php">Noticias</a></li>
-    <li><a href="/projeto-portal/pages/eventos/eventos.php">Eventos</a></li>
-    <li><a href="/projeto-portal/pages/turismo/turismo.php">Turismo</a></li>
-    <li><a href="/projeto-portal/pages/onibus/onibus.php">Horario Onibus</a></li>
-  </ul>
-</header>
+<!-- Nav -->
+<div class="index-bg-main pb-lg-3">
+  <nav class="container navbar navbar-expand-lg navbar-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand d-lg-none" href="#">
+        <img src="https://placehold.co/600x400" alt="">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav align-items-center mx-auto">
+          <li class="nav-item">
+            <a class="nav-link active text-dark" aria-current="page" href="../../../projeto-portal/index.php">Inicio</a>
+          </li>
+          <li class="nav-item dropdown"><a class="nav-link dropdown-toggle text-dark" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"> Informações </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="../../../projeto-portal/pages/onibus/onibus.php">Horário de Ônibus</a></li>
+              <li><a class="dropdown-item" href="#">História</a></li>
+            </ul>
+          </li>
+          <li class="nav-item dropdown"><a class="nav-link dropdown-toggle text-dark" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"> Guia de Eventos </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="../../../projeto-portal/pages/turismo/turismo.php">Turismo</a></li>
+              <li><a class="dropdown-item" href="../../../projeto-portal/pages/eventos/eventos.php">Eventos</a></li>
+            </ul>
+          </li>
 
-<div class="bg-light d-flex flex-column justify-content-center align-items-center">
-  <div class="container text-center">
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-      <div class="row">
+          <a class="navbar-brand d-none d-md-block" href="#">
+            <img src="https://placehold.co/120x80" alt="logo-sao-roque" class="img-fluid">
+          </a>
 
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item"><a class="nav-link active" aria-current="page" href="#"> INÍCIO </a>
-            </li>
+          <li class="nav-item">
+            <a class="nav-link active text-dark" aria-current="page" href="./../../projeto-portal/pages/noticias/noticias.php">Noticias</a>
+          </li>
 
-            <li class="nav-item dropdown"><a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"> INFORMAÇÕES </a>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">HORÁRIO DE ÔNIBUS</a></li>
-                <li><a class="dropdown-item" href="#">HISTÓRIA</a></li>
-              </ul>
-            </li>
+          <li class="nav-item dropdown"><a class="nav-link dropdown-toggle text-dark" href="" role="button" data-bs-toggle="dropdown" aria-expanded="false">Ouvidoria</a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item text-dark" href="../../../projeto-portal/pages/contato/contato.php">Contato</a></li>
+            </ul>
+          </li>
 
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"> GUIA DE EVENTOS </a>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">TURISMO</a></li>
-                <li><a class="dropdown-item" href="#">EVENTOS</a></li>
-              </ul>
-            </li>
-          </ul>
+          <li class="nav-item">
+            <a class="nav-link text-dark" href="../../../projeto-portal/pages/regrasUso/regrasUso.php">Regras de Uso</a>
+          </li>
 
-          <a class="navbar-brand" href="#"><img src="../../../projeto-portal/includes/header/assets/logo-sr-header.png" alt=" "></a>
 
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item dropdown"><a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"> OUVIDORIA </a>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">CONTATO</a></li>
-              </ul>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#"> REGRAS DE USO </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#"> QUEM SOMOS</a>
-            </li>
-          </ul>
-        </div>
+          <li class="nav-item">
+            <a class="nav-link text-dark" href="../../../projeto-portal/pages/quemSomos/quemSomos.php">Quem Somos</a>
+          </li>
+        </ul>
       </div>
-    </nav>
-  </div>
-
-
-  <div class="container text-center">
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-      <div class="row">
-        <form class="d-flex" role="search">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
-      </div>
-      <ul>
-        <li class="facebook">
-          <a href="https://facebook.com/"><i class="bi bi-facebook"></i>
-          </a>
-        </li>
-        <li class="instagram">
-          <a href="https://instagram.com/"><i class="bi bi-instagram"></i>
-          </a>
-        </li>
-        <li class="twitter">
-          <a href="https://twitter.com/"><i class="bi bi-twitter-x"></i>
-          </a>
-        </li>
-      </ul>
-    </nav>
-  </div>
+    </div>
+  </nav>
 </div>
