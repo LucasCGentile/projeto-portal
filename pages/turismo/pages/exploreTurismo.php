a43739336c25b9a451cc23edd987656d3ba7c65f
Lucas Gentile <lucasgentile@live.com>
2023-11-14 18:24:25 -0300

    add: turismo adicionado area de turismo

---
+++ pages/turismo/pages/exploreTurismo.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<?php
+include '../../includes/head/head.php'
+?>
+
+<body>
+  <?php
+  include '../../includes/header/header.php'
+  ?>
+
+  <main>
+    <h1>Explore Turismo</h1>
+    <div class="container">
+      <div class="card-turismo text-turismo">
+        <link rel="stylesheet" href="#">Lorem 1
+      </div>
+      <div class="card-turismo text-turismo">
+        <link rel="stylesheet" href="#">Lorem 2
+      </div>
+      <div class="card-turismo text-turismo">
+        <link rel="stylesheet" href="#">Lorem 3
+      </div>
+      <div class="card-turismo text-turismo">
+        <link rel="stylesheet" href="#">Lorem 4
+      </div>
+      <div class="card-turismo text-turismo">
+        <link rel="stylesheet" href="#">Lorem 5
+      </div>
+      <div class="card-turismo text-turismo">
+        <link rel="stylesheet" href="#">Lorem 6
+      </div>
+    </div>
+
+    <div class="lugares container-center">
+      <div class="exemplo" style="width: 18rem;">
+        <img src="https://placehold.co/10x10" class="card-img-top" alt="Imagem exemplo">
+        <div class="exemplo">
+          <h5 class="exemplo-title">Exemplo</h5>
+          <p class="exemplo-text">Breve resumo sobre o local</p>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">Telefone</li>
+          <li class="list-group-item">Horario</li>
+          <li class="list-group-item">Localização</li>
+        </ul>
+        <div class="exemplo-body">
+          <a href="#" class="exemplo-link">Instagram</a>
+          <a href="#" class="exemplo-link">Site oficial</a>
+        </div>
+      </div>
+    </div>
+    <div class="lugares container-center">
+      <div class="exemplo" style="width: 18rem;">
+        <img src="https://placehold.co/10x10" class="card-img-top" alt="Imagem exemplo">
+        <div class="exemplo">
+          <h5 class="exemplo-title">Exemplo</h5>
+          <p class="exemplo-text">Breve resumo sobre o local</p>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">Telefone</li>
+          <li class="list-group-item">Horario</li>
+          <li class="list-group-item">Localização</li>
+        </ul>
+        <div class="exemplo-body">
+          <a href="#" class="exemplo-link">Instagram</a>
+          <a href="#" class="exemplo-link">Site oficial</a>
+        </div>
+      </div>
+    </div>
+    <div class="lugares container-center">
+      <div class="exemplo" style="width: 18rem;">
+        <img src="https://placehold.co/10x10" class="card-img-top" alt="Imagem exemplo">
+        <div class="exemplo">
+          <h5 class="exemplo-title">Exemplo</h5>
+          <p class="exemplo-text">Breve resumo sobre o local</p>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">Telefone</li>
+          <li class="list-group-item">Horario</li>
+          <li class="list-group-item">Localização</li>
+        </ul>
+        <div class="exemplo-body">
+          <a href="#" class="exemplo-link">Instagram</a>
+          <a href="#" class="exemplo-link">Site oficial</a>
+        </div>
+      </div>
+    </div>
+    <div class="lugares container-center">
+      <div class="exemplo" style="width: 18rem;">
+        <img src="https://placehold.co/10x10" class="card-img-top" alt="Imagem exemplo">
+        <div class="exemplo">
+          <h5 class="exemplo-title">Exemplo</h5>
+          <p class="exemplo-text">Breve resumo sobre o local</p>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">Telefone</li>
+          <li class="list-group-item">Horario</li>
+          <li class="list-group-item">Localização</li>
+        </ul>
+        <div class="exemplo-body">
+          <a href="#" class="exemplo-link">Instagram</a>
+          <a href="#" class="exemplo-link">Site oficial</a>
+        </div>
+      </div>
+    </div>
+    <div class="lugares container-center">
+      <div class="exemplo" style="width: 18rem;">
+        <img src="https://placehold.co/10x10" class="card-img-top" alt="Imagem exemplo">
+        <div class="exemplo">
+          <h5 class="exemplo-title">Exemplo</h5>
+          <p class="exemplo-text">Breve resumo sobre o local</p>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">Telefone</li>
+          <li class="list-group-item">Horario</li>
+          <li class="list-group-item">Localização</li>
+        </ul>
+        <div class="exemplo-body">
+          <a href="#" class="exemplo-link">Instagram</a>
+          <a href="#" class="exemplo-link">Site oficial</a>
+        </div>
+      </div>
+    </div>
+    <div class="lugares container-center">
+      <div class="exemplo" style="width: 18rem;">
+        <img src="https://placehold.co/10x10" class="card-img-top" alt="Imagem exemplo">
+        <div class="exemplo">
+          <h5 class="exemplo-title">Exemplo</h5>
+          <p class="exemplo-text">Breve resumo sobre o local</p>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">Telefone</li>
+          <li class="list-group-item">Horario</li>
+          <li class="list-group-item">Localização</li>
+        </ul>
+        <div class="exemplo-body">
+          <a href="#" class="exemplo-link">Instagram</a>
+          <a href="#" class="exemplo-link">Site oficial</a>
+        </div>
+      </div>
+    </div>
+  </main>
+
+  <?php
+  include '../../includes/footer/footer.php'
+  ?>
+</body>
+
+</html>
