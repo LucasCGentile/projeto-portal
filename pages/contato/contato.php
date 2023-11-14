c480f014d8a119d2f31e3f32bb32f669acb9b54f
Lucas Gentile <lucasgentile@live.com>
2023-11-14 16:47:27 -0300

    add: contato adiciona area de contato adicionado css de contato

---
+++ pages/contato/contato.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+
+<?php
+include '../../includes/head/head.php'
+?>
+
+<body>
+  <?php
+  include '../../includes/header/header.php'
+  ?>
+  <div class="ouvidoria container text-center">
+    <div class="card text-bg-dark sobreposto">
+      <img src="https://placehold.co/1195x240" class="card-img" alt="...">
+      <div class="card-img-overlay">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small>Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+  <h1 class="container text-center">
+    OUVIDORIA E CONTATOS
+  </h1>
+  <div class="ouvidoria container">
+
+
+
+    <div class="ouv-miolo">
+
+      <div class=" tabelaouv card" style="width: 18rem;">
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item"> Ouvidoria</li>
+          <li class="list-group-item"> Nosso Contato</li>
+          <li class="list-group-item"> Perguntas Frequentes</li>
+          <li class="list-group-item"> Nós queremos sua opnião!</li>
+          <li class="list-group-item"> Cabaré da Sheila 50% Off</li>
+        </ul>
+      </div>
+      <section class="infouvidoria container text-center">
+        <article class="container text-center">
+          <section class="iconesouv text-center img-fluid">
+            <img src="https://placehold.co/70x70" alt="">
+            <img src="https://placehold.co/70x70" alt="">
+            <img src="https://placehold.co/70x70" alt="">
+            <img src="https://placehold.co/70x70" alt="">
+            <img src="https://placehold.co/70x70" alt="">
+            <img src="https://placehold.co/70x70" alt="">
+          </section>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab vitae doloremque hic illum beatae et nemo! Cupiditate dolorum debitis rem quaerat dignissimos deleniti iure maxime sint, exercitationem quis neque laborum.
+            Quae doloremque temporibus accusantium quos minima nisi delectus saepe, expedita similique, consequuntur aut fugit ipsum impedit eos atque modi! Voluptatibus ipsam vitae sunt temporibus exercitationem aliquam laboriosam mollitia, maiores adipisci!</p>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus numquam fugit eum ad placeat, temporibus provident impedit obcaecati modi, deserunt dolorem natus optio dicta deleniti expedita! Laborum, perspiciatis? Sit, veritatis.
+            Quod ut earum non accusamus aut officia reiciendis quasi cum tenetur, eius beatae labore facere consequuntur aliquam, deserunt temporibus at mollitia expedita ab quas cumque exercitationem pariatur nemo praesentium! Excepturi.
+          </p>
+
+
+        </article>
+      </section>
+
+    </div>
+
+    <h1 class="text-center">
+      INFORMAÇOES ADICIONAIS
+    </h1>
+
+    <p class="text-center">
+      <strong>contato:</strong> (11)9999999999
+    </p>
+
+    <p class="text-center">
+      <strong>Email:</strong> ursinhoscarinhosos@gmail.com
+    </p>
+
+    <p class="text-center">
+      <strong>Endereço:</strong> Ali do Lado
+    </p>
+  </div>
+
+  <?php
+  include '../../includes/footer/footer.php'
+  ?>
+</body>
+
+</html>
