e15de6473b11cd8bdc2854a4bf411e41f8e95231
Lucas Gentile <lucasgentile@live.com>
2023-11-14 18:27:34 -0300

    update: area de normas de uso refeita

--- pages/regras/regras.php
+++ pages/regras/regras.php
@@ -11,78 +11,48 @@ include '../../includes/head/head.php'
   include '../../includes/header/header.php'
   ?>
 
-  <h1 class="container text-center">HISTORIA DA NOSSA CIDADE</h1>
+  <h1 class="text-center">NORMAS DE USO</h1>
 
-  <section class="histdesr container text-center">
+  <div class="normas container text-center">
+    <h1>
+      1. Norma Tchubiraumdaum
+    </h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto labore illum aliquam autem libero omnis pariatur, sapiente earum quis impedit, iure odio, recusandae voluptate sequi maxime facere voluptatum magnam corporis.
+      Cupiditate incidunt temporibus provident distinctio rem ea voluptatum praesentium amet! Magnam architecto expedita eaque animi sint, eligendi ullam laudantium consequuntur, nam natus officiis fugit quis culpa perspiciatis ea optio et!
+      Natus in officia maiores ut officiis necessitatibus cum neque iusto reiciendis et, est eum voluptas quibusdam ea inventore. Sequi error hic soluta vel sint dolorum iste nulla nihil beatae esse.
+      Sit nihil et placeat mollitia possimus, distinctio cupiditate doloribus eum, quos est ut accusamus doloremque necessitatibus, consequuntur accusantium soluta fugit repellat consequatur omnis dolorem aperiam labore magnam temporibus! Quisquam, placeat!
+      Modi cum atque reiciendis ex cupiditate saepe laboriosam laborum ducimus deleniti harum ab perspiciatis laudantium, sunt et sed natus iure odit itaque, perferendis molestiae similique? Quam aspernatur non obcaecati repellat.
+    </p>
 
-    <img src="https://placehold.co/200x200" alt="">
+    <h1>
+      2. Norma Tchacatchaca na butchaca
+    </h1>
 
-    <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos odio laborum vero quam commodi aperiam fugit quaerat perspiciatis voluptatum reprehenderit expedita, sint nihil molestiae. Officia illo laudantium itaque doloremque molestias.
-      Quo eum, aliquid optio qui ex maiores doloremque vero debitis eaque! Iste necessitatibus id quas ea? Magnam vero harum et fuga possimus mollitia consequuntur iure, id, eaque ea assumenda molestiae.
-      Recusandae sit autem, eos omnis dolorem similique distinctio mollitia maxime! Officiis iste corrupti natus quidem, accusantium, et inventore modi rem doloribus voluptate dignissimos fugit hic ex, labore cumque ipsa velit?</p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis repellat molestiae, soluta in at eum molestias aliquid voluptates quis, fuga sit consequatur ex quasi voluptate quisquam. Hic nam et labore. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas consequatur autem quod, cumque eius deleniti inventore! Fugiat deserunt nam aut animi, vero earum tempora? Quisquam, iure! Numquam voluptas eum reprehenderit.
+      Officiis placeat nemo ipsam exercitationem adipisci ipsa, expedita debitis asperiores esse deserunt molestias commodi vel atque dignissimos id! Rem fugiat quod illum dolores odit beatae consequuntur praesentium similique, quos omnis.
+      Quidem, natus ipsam! Quas nesciunt fuga officiis id iusto mollitia omnis facere ullam harum saepe velit minus nam suscipit ipsa, reiciendis corrupti. Quasi id placeat nisi beatae, ex possimus culpa.
+      Ullam similique labore, voluptatum quae nostrum maxime architecto sint cumque. Aliquam quis itaque porro saepe earum recusandae exercitationem a mollitia est nisi soluta doloremque, temporibus error sunt iusto, fuga accusamus!
+      Voluptatum dolorem at odit itaque eius alias fugiat vel rem veritatis, est obcaecati aperiam animi, tenetur dolorum iste atque omnis ea dolore impedit nulla! Suscipit enim exercitationem cumque amet delectus.
+    </p>
 
-
-  </section>
-
-  <section class="histdesr container text-center">
-
-    <img src="https://placehold.co/200x200" alt="">
-
-    <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos odio laborum vero quam commodi aperiam fugit quaerat perspiciatis voluptatum reprehenderit expedita, sint nihil molestiae. Officia illo laudantium itaque doloremque molestias.
-      Quo eum, aliquid optio qui ex maiores doloremque vero debitis eaque! Iste necessitatibus id quas ea? Magnam vero harum et fuga possimus mollitia consequuntur iure, id, eaque ea assumenda molestiae.
-      Recusandae sit autem, eos omnis dolorem similique distinctio mollitia maxime! Officiis iste corrupti natus quidem, accusantium, et inventore modi rem doloribus voluptate dignissimos fugit hic ex, labore cumque ipsa velit?</p>
-
-
-  </section>
-
-  <div class="imghist container text-center">
-    <img src="https://placehold.co/200x200" class="rouded" alt="">
-    <img src="https://placehold.co/200x200" alt="">
-    <img src="https://placehold.co/200x200" alt="">
-  </div>
-
-  <div class="container text-center">
-
-    <section class="histpt1 container text-center ">
-
-      <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos odio laborum vero quam commodi aperiam fugit quaerat perspiciatis voluptatum reprehenderit expedita, sint nihil molestiae. Officia illo laudantium itaque doloremque molestias.
-        Quo eum, aliquid optio qui ex maiores doloremque vero debitis eaque! Iste necessitatibus id quas ea? Magnam vero harum et fuga possimus mollitia consequuntur iure, id, eaque ea assumenda molestiae.
-        Recusandae sit autem, eos omnis dolorem similique distinctio mollitia maxime! Officiis iste corrupti natus quidem, accusantium, et inventore modi rem doloribus voluptate dignissimos fugit hic ex, labore cumque ipsa velit?</p>
-      <img src="https://placehold.co/200x200" alt="">
-
-
-    </section>
-
-
-    <section class="histpt1 container-hist text-center">
-
-      <img src="https://placehold.co/200x200" alt="">
-
-      <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos odio laborum vero quam commodi aperiam fugit quaerat perspiciatis voluptatum reprehenderit expedita, sint nihil molestiae. Officia illo laudantium itaque doloremque molestias.
-        Quo eum, aliquid optio qui ex maiores doloremque vero debitis eaque! Iste necessitatibus id quas ea? Magnam vero harum et fuga possimus mollitia consequuntur iure, id, eaque ea assumenda molestiae.
-        Recusandae sit autem, eos omnis dolorem similique distinctio mollitia maxime! Officiis iste corrupti natus quidem, accusantium, et inventore modi rem doloribus voluptate dignissimos fugit hic ex, labore cumque ipsa velit?</p>
-
-
-    </section>
+    <h1>
+      3. Norma Biruleibe
+    </h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio tenetur maiores fugit corrupti, dolor, quia deleniti, ut laborum doloremque quos pariatur totam sapiente hic ab assumenda nobis id perspiciatis illum.
+      Libero deleniti magnam, debitis non officiis, harum, ipsa eos veritatis suscipit ratione illum? Facilis deserunt recusandae eos aperiam velit vitae accusamus, cumque, ut amet enim necessitatibus doloremque magni! Quibusdam, obcaecati?
+      Neque, ducimus debitis modi numquam quisquam maxime iusto est, asperiores consequatur accusamus doloribus fuga esse quod perspiciatis deleniti architecto et quibusdam sed praesentium tempora, minus tenetur. Alias laudantium numquam ab!
+      Repudiandae, architecto. Qui autem esse deserunt ea ipsum mollitia ad, atque repellat repellendus omnis asperiores commodi, voluptas natus nemo ipsam in quo deleniti corrupti sunt aliquid! Veritatis temporibus officiis iure?
+      At quaerat rerum dolorem maxime vero qui quam excepturi velit consectetur, aliquid eius architecto eveniet obcaecati cupiditate eos ad cum error cumque reprehenderit explicabo eum! Suscipit deleniti obcaecati voluptates consequuntur?
+    </p>
   </div>
 
 
-  <footer>
-    <small>
-      &copy; Por enquantpo NADA
-    </small>
-
-  </footer>
-
-
-
-
-
-
-
-
-
-  </div>
+  <?php
+  include '../../includes/footer/footer.php'
+  ?>
 </body>
 
 </html>
