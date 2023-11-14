c4f6d90e5f01dae39da99d9efcf774802c6410ba
Lucas Gentile <lucasgentile@live.com>
2023-11-14 00:00:50 -0300

    add: criado area de turismo

---
+++ pages/turismo/turismo.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+include '../../includes/head/head.php'
+?>
+
+<body>
+  <?php
+  include '../../includes/header/header.php'
+  ?>
+
+  <main>
+    <h1>Area em Manutencao</h1>
+  </main>
+
+  <?php
+  include '../../includes/footer/footer.php'
+  ?>
+</body>
+
+</html>
