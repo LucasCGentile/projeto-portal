91edc7d66d2ebde42662a02920f8b18659a1c501
Lucas Gentile <lucasgentile@live.com>
2023-11-14 16:55:54 -0300

    add: regras adicionado area de regras de uso ao site

---
+++ pages/regras/regras.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="PT-Br">
+
+<?php
+include '../../includes/head/head.php'
+?>
+
+<body>
+
+  <?php
+  include '../../includes/header/header.php'
+  ?>
+
+  <h1 class="container text-center">HISTORIA DA NOSSA CIDADE</h1>
+
+  <section class="histdesr container text-center">
+
+    <img src="https://placehold.co/200x200" alt="">
+
+    <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos odio laborum vero quam commodi aperiam fugit quaerat perspiciatis voluptatum reprehenderit expedita, sint nihil molestiae. Officia illo laudantium itaque doloremque molestias.
+      Quo eum, aliquid optio qui ex maiores doloremque vero debitis eaque! Iste necessitatibus id quas ea? Magnam vero harum et fuga possimus mollitia consequuntur iure, id, eaque ea assumenda molestiae.
+      Recusandae sit autem, eos omnis dolorem similique distinctio mollitia maxime! Officiis iste corrupti natus quidem, accusantium, et inventore modi rem doloribus voluptate dignissimos fugit hic ex, labore cumque ipsa velit?</p>
+
+
+  </section>
+
+  <section class="histdesr container text-center">
+
+    <img src="https://placehold.co/200x200" alt="">
+
+    <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos odio laborum vero quam commodi aperiam fugit quaerat perspiciatis voluptatum reprehenderit expedita, sint nihil molestiae. Officia illo laudantium itaque doloremque molestias.
+      Quo eum, aliquid optio qui ex maiores doloremque vero debitis eaque! Iste necessitatibus id quas ea? Magnam vero harum et fuga possimus mollitia consequuntur iure, id, eaque ea assumenda molestiae.
+      Recusandae sit autem, eos omnis dolorem similique distinctio mollitia maxime! Officiis iste corrupti natus quidem, accusantium, et inventore modi rem doloribus voluptate dignissimos fugit hic ex, labore cumque ipsa velit?</p>
+
+
+  </section>
+
+  <div class="imghist container text-center">
+    <img src="https://placehold.co/200x200" class="rouded" alt="">
+    <img src="https://placehold.co/200x200" alt="">
+    <img src="https://placehold.co/200x200" alt="">
+  </div>
+
+  <div class="container text-center">
+
+    <section class="histpt1 container text-center ">
+
+      <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos odio laborum vero quam commodi aperiam fugit quaerat perspiciatis voluptatum reprehenderit expedita, sint nihil molestiae. Officia illo laudantium itaque doloremque molestias.
+        Quo eum, aliquid optio qui ex maiores doloremque vero debitis eaque! Iste necessitatibus id quas ea? Magnam vero harum et fuga possimus mollitia consequuntur iure, id, eaque ea assumenda molestiae.
+        Recusandae sit autem, eos omnis dolorem similique distinctio mollitia maxime! Officiis iste corrupti natus quidem, accusantium, et inventore modi rem doloribus voluptate dignissimos fugit hic ex, labore cumque ipsa velit?</p>
+      <img src="https://placehold.co/200x200" alt="">
+
+
+    </section>
+
+
+    <section class="histpt1 container-hist text-center">
+
+      <img src="https://placehold.co/200x200" alt="">
+
+      <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos odio laborum vero quam commodi aperiam fugit quaerat perspiciatis voluptatum reprehenderit expedita, sint nihil molestiae. Officia illo laudantium itaque doloremque molestias.
+        Quo eum, aliquid optio qui ex maiores doloremque vero debitis eaque! Iste necessitatibus id quas ea? Magnam vero harum et fuga possimus mollitia consequuntur iure, id, eaque ea assumenda molestiae.
+        Recusandae sit autem, eos omnis dolorem similique distinctio mollitia maxime! Officiis iste corrupti natus quidem, accusantium, et inventore modi rem doloribus voluptate dignissimos fugit hic ex, labore cumque ipsa velit?</p>
+
+
+    </section>
+  </div>
+
+
+  <footer>
+    <small>
+      &copy; Por enquantpo NADA
+    </small>
+
+  </footer>
+
+
+
+
+
+
+
+
+
+  </div>
+</body>
+
+</html>
