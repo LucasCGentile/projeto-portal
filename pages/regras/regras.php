a89af33ea3557e6edca63b82393abd1bb164d1bc
isaiaax <isaiasc963@gmail.com>
2023-12-03 19:57:17 -0300

    margin do conteudo para o footer

--- pages/regras/regras.php
+++ pages/regras/regras.php
@@ -15,20 +15,28 @@ include '../../includes/head/head.php'
     <h1 class="text-center">NORMAS DE USO</h1>
 
 
-    <div class="normas container text-center">
+    <div class="normas container text-center mb-5">
         <h1>
             1. ULTILIZAÇÃO
         </h1>
         <p>
-            O VISITANTE ao utilizar o website dos <strong>Ursinhos carinhosos da VINGANÇA"</strong>, concorda integralmente em:
+            O VISITANTE ao utilizar o website dos <strong>Ursinhos carinhosos da VINGANÇA"</strong>, concorda
+            integralmente em:
 
-            De nenhuma forma ou meio realizar qualquer tipo de ação que tente invadir, hacker, destruir ou prejudicar a estrutura do site, plataforma da EMPRESA ou de seus parceiros comerciais. Incluindo-se, mas não se limitando, ao envio de vírus de computador, de ataques de DDOS, de acesso indevido por falhas da mesma ou quaisquer outras forma e meio.
+            De nenhuma forma ou meio realizar qualquer tipo de ação que tente invadir, hacker, destruir ou prejudicar a
+            estrutura do site, plataforma da EMPRESA ou de seus parceiros comerciais. Incluindo-se, mas não se
+            limitando, ao envio de vírus de computador, de ataques de DDOS, de acesso indevido por falhas da mesma ou
+            quaisquer outras forma e meio.
 
-            De não realizar divulgação indevida nos comentários do site de conteúdo de SPAM, empresas concorrentes, vírus, conteúdo que não possua direitos autorais ou quaisquer outros que não seja pertinente a discussão daquele texto, vídeo ou imagem.
+            De não realizar divulgação indevida nos comentários do site de conteúdo de SPAM, empresas concorrentes,
+            vírus, conteúdo que não possua direitos autorais ou quaisquer outros que não seja pertinente a discussão
+            daquele texto, vídeo ou imagem.
 
-            Da proibição em reproduzir qualquer conteúdo do site ou plataforma sem autorização expressa, podendo responder civil e criminalmente pelo mesmo.
+            Da proibição em reproduzir qualquer conteúdo do site ou plataforma sem autorização expressa, podendo
+            responder civil e criminalmente pelo mesmo.
 
-            Com a Política de Privacidade do site, assim como tratamos os dados referentes ao cadastro e visita no site, podendo a qualquer momento e forma, requerer a exclusão dos mesmos, através do formulário de contato.
+            Com a Política de Privacidade do site, assim como tratamos os dados referentes ao cadastro e visita no site,
+            podendo a qualquer momento e forma, requerer a exclusão dos mesmos, através do formulário de contato.
         </p>
 
         <h1>
@@ -36,18 +44,25 @@ include '../../includes/head/head.php'
         </h1>
 
         <p>
-            Todo o conteúdo do site é protegido por direitos autorais, e seu uso, cópia, transmissão, venda, cessão ou revenda, deve seguir a lei brasileira, tendo a EMPRESA todos os seus direitos reservados, e não permitindo a cópia ou utilização de nenhuma forma e meio, sem autorização expressa e por escrita da mesma.
-            <strong>A VINGANÇA DOS URSINHOS CARINHOSOS</strong>poderá em casos concretos permitir pontualmente exceções a este direito, que serão claramente destacados no mesmo, com a forma e permissão de uso do conteúdo protegido. Este direito é revogável e limitado as especificações de cada caso.
+            Todo o conteúdo do site é protegido por direitos autorais, e seu uso, cópia, transmissão, venda, cessão ou
+            revenda, deve seguir a lei brasileira, tendo a EMPRESA todos os seus direitos reservados, e não permitindo a
+            cópia ou utilização de nenhuma forma e meio, sem autorização expressa e por escrita da mesma.
+            <strong>A VINGANÇA DOS URSINHOS CARINHOSOS</strong>poderá em casos concretos permitir pontualmente exceções
+            a este direito, que serão claramente destacados no mesmo, com a forma e permissão de uso do conteúdo
+            protegido. Este direito é revogável e limitado as especificações de cada caso.
         </p>
 
         <h1>
             3. ACESSO AO SITE
         </h1>
         <p>
-            O Site e plataforma funcionam normalmente 24 (vinte e quatro) horas por dia, porém podem ocorrer pequenas interrupções de forma temporária para ajustes, manutenção, mudança de servidores, falhas técnicas ou por ordem de força maior, que podem deixar o site indisponível por tempo limitado.
-            <strong>A VINGANÇA DOS URSINHOS CARINHOSOS</strong> não se responsabiliza por nenhuma perda de oportunidade ou prejuízos que esta indisponibilidade temporária possa gerar aos usuários.
+            O Site e plataforma funcionam normalmente 24 (vinte e quatro) horas por dia, porém podem ocorrer pequenas
+            interrupções de forma temporária para ajustes, manutenção, mudança de servidores, falhas técnicas ou por
+            ordem de força maior, que podem deixar o site indisponível por tempo limitado.
+            <strong>A VINGANÇA DOS URSINHOS CARINHOSOS</strong> não se responsabiliza por nenhuma perda de oportunidade
+            ou prejuízos que esta indisponibilidade temporária possa gerar aos usuários.
         </p>
-        
+
     </div>
 
 
