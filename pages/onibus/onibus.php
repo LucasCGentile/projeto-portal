9aa25f8379a0d79313349b427680b75fecdc8e37
isaiaax <isaiasc963@gmail.com>
2023-12-04 21:15:14 -0300

    update final da pagina onibus

--- pages/onibus/onibus.php
+++ pages/onibus/onibus.php
@@ -13,7 +13,7 @@ include '../../includes/head/head.php';
     <main>
         <article class="container mb-5">
             <div>
-                <h1 class="title-header-bus bg-transparent">Horários de Ônibus <i class="bi bi-bus-front-fill"></i></h1>
+                <h1 class="title-header-bus">Horários de Ônibus <i class="bi bi-bus-front-fill"></i></h1>
             </div>
             <div class="row">
                 <h2 class="mt-5">Linhas Municipais</h2>
@@ -22,7 +22,8 @@ include '../../includes/head/head.php';
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6 mt-3">
-                    <div class="card-link card-home-bus rounded"><a class="card-link-home-bus" href="./carmo.php">Carmo via Ponta Porã<i class="bi bi-chevron-right float-end"></i></a></div>
+                    <div class="card-link card-home-bus rounded"><a class="card-link-home-bus" href="./carmo.php">Carmo
+                            via Ponta Porã<i class="bi bi-chevron-right float-end"></i></a></div>
                 </div>
                 <div class="col-lg-4 col-md-6 mt-3">
                     <div class="card-link card-home-bus rounded"><a class="card-link-home-bus" href="./gabriel.php">Gabriel Piza<i class="bi bi-chevron-right float-end"></i></a></div>
@@ -32,7 +33,8 @@ include '../../includes/head/head.php';
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6 mt-3">
-                    <div class="card-link card-home-bus rounded"><a class="card-link-home-bus" href="./brasilia.php">Jd. Brasília - São Rafael<i class="bi bi-chevron-right float-end"></i></a></div>
+                    <div class="card-link card-home-bus rounded"><a class="card-link-home-bus" href="./brasilia.php">Jd.
+                            Brasília - São Rafael<i class="bi bi-chevron-right float-end"></i></a></div>
                 </div>
                 <div class="col-lg-4 col-md-6 mt-3">
                     <div class="card-link card-home-bus rounded"><a class="card-link-home-bus" href="./mailasqui_vgp.php">Mailiasqui - Vargem Grande<i class="bi bi-chevron-right float-end"></i></a></div>
@@ -51,16 +53,19 @@ include '../../includes/head/head.php';
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6 mt-3">
-                    <div class="card-link card-home-bus rounded"><a class="card-link-home-bus" href="./paisagem.php">Paisagem Colonial<i class="bi bi-chevron-right float-end"></i></a></div>
+                    <div class="card-link card-home-bus rounded"><a class="card-link-home-bus" href="./paisagem.php">Paisagem Colonial<i class="bi bi-chevron-right float-end"></i></a>
+                    </div>
                 </div>
                 <div class="col-lg-4 col-md-6 mt-3">
-                    <div class="card-link card-home-bus rounded"><a class="card-link-home-bus" href="./pq_alianca.php">Parque Aliança<i class="bi bi-chevron-right float-end"></i></a></div>
+                    <div class="card-link card-home-bus rounded"><a class="card-link-home-bus" href="./pq_alianca.php">Parque Aliança<i class="bi bi-chevron-right float-end"></i></a>
+                    </div>
                 </div>
                 <div class="col-lg-4 col-md-6 mt-3">
                     <div class="card-link card-home-bus rounded"><a class="card-link-home-bus" href="./planalto.php">Planalto Verde<i class="bi bi-chevron-right float-end"></i></a></div>
                 </div>
                 <div class="col-lg-4 col-md-6 mt-3">
-                    <div class="card-link card-home-bus rounded"><a class="card-link-home-bus" href="./ponte_lavrada.php">Ponte Lavrada<i class="bi bi-chevron-right float-end"></i></a></div>
+                    <div class="card-link card-home-bus rounded"><a class="card-link-home-bus" href="./ponte_lavrada.php">Ponte Lavrada<i class="bi bi-chevron-right float-end"></i></a>
+                    </div>
                 </div>
                 <div class="col-lg-4 col-md-6 mt-3">
                     <div class="card-link card-home-bus rounded"><a class="card-link-home-bus" href="./saboo.php">Saboó<i class="bi bi-chevron-right float-end"></i></a>
@@ -70,7 +75,8 @@ include '../../includes/head/head.php';
                     <div class="card-link card-home-bus rounded"><a class="card-link-home-bus" href="./st_antonio.php">Santo Antônio<i class="bi bi-chevron-right float-end"></i></a></div>
                 </div>
                 <div class="col-lg-4 col-md-6 mt-3">
-                    <div class="card-link card-home-bus rounded"><a class="card-link-home-bus" href="./sao_joao.php">São João Novo<i class="bi bi-chevron-right float-end"></i></a></div>
+                    <div class="card-link card-home-bus rounded"><a class="card-link-home-bus" href="./sao_joao.php">São
+                            João Novo<i class="bi bi-chevron-right float-end"></i></a></div>
                 </div>
                 <div class="col-lg-4 col-md-6 mt-3">
                     <div class="card-link card-home-bus rounded"><a class="card-link-home-bus" href="./sorocamirim.php">Sorocamirim - Pavão<i class="bi bi-chevron-right float-end"></i></a></div>
@@ -88,7 +94,8 @@ include '../../includes/head/head.php';
                     <div class="card-link card-home-bus rounded"><a class="card-link-home-bus" href="./aluminio.php">Alumínio<i class="bi bi-chevron-right float-end"></i></a></div>
                 </div>
                 <div class="col-lg-4 col-md-6 mt-3">
-                    <div class="card-link card-home-bus rounded"><a class="card-link-home-bus" href="./aracariguama.php">Araçariguama<i class="bi bi-chevron-right float-end"></i></a></div>
+                    <div class="card-link card-home-bus rounded"><a class="card-link-home-bus" href="./aracariguama.php">Araçariguama<i class="bi bi-chevron-right float-end"></i></a>
+                    </div>
                 </div>
                 <div class="col-lg-4 col-md-6 mt-3">
                     <div class="card-link card-home-bus rounded"><a class="card-link-home-bus" href="./cotia.php">Cotia<i class="bi bi-chevron-right float-end"></i></a></div>
