3259be8b7867665e5ffe06fc7fa9eb17106065fc
Lucas Gentile <lucasgentile@live.com>
2023-12-07 10:26:20 -0300

    update: path alterado para melhor compatibilidade

--- includes/head/head.php
+++ includes/head/head.php
@@ -2,7 +2,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php echo $titulo ?></title>
-  <link rel="shortcut icon" href="../../../projeto-portal/includes/head/assets/images/favicon.ico" type="image/x-icon">
+  <link rel="shortcut icon" href="/projeto-portal/includes/head/assets/images/favicon.ico" type="image/x-icon">
   <!-- Conexao Bootstrap -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <!-- Conexao Bootstrap Icons -->
@@ -13,5 +13,5 @@
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700;800&display=swap" rel="stylesheet">
 
   <!-- CSS -->
-  <link rel="stylesheet" href="../../../projeto-portal/css/style.css">
+  <link rel="stylesheet" href="/projeto-portal/css/style.css">
 </head>
--- includes/header/header.php
+++ includes/header/header.php
@@ -6,8 +6,8 @@ $page = isset($_GET['page']) ? $_GET['page'] : 'home';
 <div class="index-bg-main pb-lg-3">
   <nav class="container navbar navbar-expand-lg ">
     <div class="container-fluid fw-bold">
-      <a class="navbar-brand d-lg-none" href="../../../projeto-portal/index.php">
-        <img src="../../../projeto-portal/includes/header/assets/logo-sr-header.png" alt="img-fluid">
+      <a class="navbar-brand d-lg-none" href="/projeto-portal/index.php">
+        <img src="/projeto-portal/includes/header/assets/logo-sr-header.png" alt="img-fluid">
       </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -15,51 +15,51 @@ $page = isset($_GET['page']) ? $_GET['page'] : 'home';
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav align-items-center mx-auto">
           <li class="nav-item">
-            <a class="nav-link link <?= ($page == 'home') ? 'active' : '' ?>" aria-current="page" href="../../../projeto-portal/index.php?page=home">Inicio</a>
+            <a class="nav-link link <?= ($page == 'home') ? 'active' : '' ?>" aria-current="page" href="/projeto-portal/index.php?page=home">Inicio</a>
           </li>
 
           <li class="nav-item dropdown <?= ($page == 'informacoes') ? 'active' : '' ?>"><a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"> Informações </a>
             <ul class="dropdown-menu dropdown-content">
-              <li><a class="dropdown-item fw-bold" href="../../../projeto-portal/pages/onibus/onibus.php?page=informacoes">Horário de
+              <li><a class="dropdown-item fw-bold" href="/projeto-portal/pages/onibus/onibus.php?page=informacoes">Horário de
                   Ônibus</a></li>
-              <li><a class="dropdown-item fw-bold" href="../../../projeto-portal/pages/historia/historia.php?page=informacoes">História</a>
+              <li><a class="dropdown-item fw-bold" href="/projeto-portal/pages/historia/historia.php?page=informacoes">História</a>
               </li>
-              <li><a class="dropdown-item fw-bold" href="../../../projeto-portal/pages/historia/galeriapref.php?page=informacoes">Prefeitos</a>
+              <li><a class="dropdown-item fw-bold" href="/projeto-portal/pages/historia/galeriapref.php?page=informacoes">Prefeitos</a>
               </li>
             </ul>
           </li>
 
           <li class="nav-item dropdown <?= ($page == 'eventos') ? 'active' : '' ?>"><a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"> Guia de Eventos </a>
             <ul class="dropdown-menu dropdown-content">
-              <li><a class="dropdown-item fw-bold" href="../../../projeto-portal/pages/turismo/turismo.php?page=eventos">Turismo</a>
+              <li><a class="dropdown-item fw-bold" href="/projeto-portal/pages/turismo/turismo.php?page=eventos">Turismo</a>
               </li>
-              <li><a class="dropdown-item fw-bold" href="../../../projeto-portal/pages/eventos/eventos.php?page=eventos">Eventos</a>
+              <li><a class="dropdown-item fw-bold" href="/projeto-portal/pages/eventos/eventos.php?page=eventos">Eventos</a>
               </li>
             </ul>
           </li>
 
-          <a class="navbar-brand d-none d-lg-block" href="../../../projeto-portal/index.php">
-            <img src="../../../projeto-portal/includes/header/assets/logo-sr-header.png" alt="logo-sao-roque" class="img-fluid">
+          <a class="navbar-brand d-none d-lg-block" href="/projeto-portal/index.php">
+            <img src="/projeto-portal/includes/header/assets/logo-sr-header.png" alt="logo-sao-roque" class="img-fluid">
           </a>
 
           <li class="nav-item">
-            <a class="nav-link link" aria-current="page" href="../../../projeto-portal/pages/noticias/noticias.php">Noticias</a>
+            <a class="nav-link link" aria-current="page" href="/projeto-portal/pages/noticias/noticias.php">Noticias</a>
           </li>
 
           <li class="nav-item dropdown <?= ($page == 'contato') ? 'active' : '' ?>"><a class="nav-link dropdown-toggle" href="" role="button" data-bs-toggle="dropdown" aria-expanded="false">Ouvidoria</a>
             <ul class="dropdown-menu dropdown-content">
-              <li><a class="dropdown-item fw-bold" href="../../../projeto-portal/pages/contato/contato.php?page=contato">Contato</a>
+              <li><a class="dropdown-item fw-bold" href="/projeto-portal/pages/contato/contato.php?page=contato">Contato</a>
               </li>
             </ul>
           </li>
 
           <li class="nav-item">
-            <a class="nav-link link <?= ($page == 'regrasDeUso') ? 'active' : '' ?>" aria-current="page" href="../../../projeto-portal/pages/regras/regras.php?page=regrasDeUso">Regras de Uso</a>
+            <a class="nav-link link <?= ($page == 'regrasDeUso') ? 'active' : '' ?>" aria-current="page" href="/projeto-portal/pages/regras/regras.php?page=regrasDeUso">Regras de Uso</a>
           </li>
 
 
           <li class="nav-item">
-            <a class="nav-link link <?= ($page == 'quemSomos') ? 'active' : '' ?>" href="../../../projeto-portal/pages/quemSomos/quemSomos.php?page=quemSomos">Quem Somos</a>
+            <a class="nav-link link <?= ($page == 'quemSomos') ? 'active' : '' ?>" href="/projeto-portal/pages/quemSomos/quemSomos.php?page=quemSomos">Quem Somos</a>
           </li>
         </ul>
       </div>
